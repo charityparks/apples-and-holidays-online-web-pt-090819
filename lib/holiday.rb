60527a97fdcc47c0c9f8b2213766e9b5efa0ab83
Charity Parks <tuppercharity@gmail.com>
2019-09-23 21:46:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+   holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+      holiday.each do |days, supplies|
+        puts "  #{days.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+      end
+  end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
